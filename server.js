cc26be001fb4ef11958e59a5879c8f2d528294c3
Paul Arthur <paularthurjnr611@gmail.com>
2023-09-11 01:02:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -423,6 +423,6 @@ app.use((err, req, res, next) => {
 })
 
 // listen
-app.listen(8182, () => {
-    console.log('Listening on 8182')
+app.listen(3000, () => {
+    console.log('Listening on 3000')
 })
